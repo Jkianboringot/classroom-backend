ceb49a24cae584a7722b68745b1efac9118e5a44
jkian <slyrics443@gmail.com>
2026-03-28 17:50:36 +0800

    subjects controller

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import {PORT} from '../config/env.js'
-
+import { subjects } from "./db/schema/app.js";
+import subjectsRouter from './routes/subject'
 
 const app = express();
 
 app.use(express.json())
-//ok so the parameter in this callback where req and res are suppose to be is order base, meaning 
-// you can name it anything you want, the first param will always be req once, and the second is the res once
-// i was about to ask ai it but i figure it out myself fuck, the more you know
+
+
+app.use('api/subjects',subjectsRouter)
+
 app.get('/',(fuck,shit)=>{
     shit.send('fuckyou bitch')
 })
