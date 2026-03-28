7edbade399a6c0bdc2f5071704c2bbc232dd8489
jkian <slyrics443@gmail.com>
2026-03-28 13:59:27 +0800

    created server

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {PORT} from '../config/env.js'
 
 const app = express();
 
-
+app.use(express.json())
 //ok so the parameter in this callback where req and res are suppose to be is order base, meaning 
 // you can name it anything you want, the first param will always be req once, and the second is the res once
 // i was about to ask ai it but i figure it out myself fuck, the more you know
